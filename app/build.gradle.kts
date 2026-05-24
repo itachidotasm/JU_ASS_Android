accbc6be21662483ee13549ded105f7dd7587da7
itachi <sharmaps1758@gmail.com>
2026-05-24 09:19:09 +0000

    Fix material icons unresolved reference

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     
     // Retrofit & Gson for JSON parsing
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
